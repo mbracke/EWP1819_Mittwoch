beefdb834c94d9981e7b96cf444058ab43099abb
Nathanael Stelzner <nstelzne@tl-agent3.mathematik.uni-kl.de>
2019-01-04 08:59:09 +0100

    tolle funktion

--- inClass_Students/test_fun.py
+++ inClass_Students/test_fun.py
@@ -1,9 +1,18 @@
 import numpy as np
 
 def f_1D_scipy(x):
+    '''
+    Funktion 1D
+    '''
     f = np.sin(x) ** 3.0
     return f
 
 def dfdx_1D_scipy(x):
+    '''
+    
+    '''
     df = 3.0 * (np.sin(x) **2.0) * np.cos(x)
     return df
+
+def f(x):
+    return x**2
